031391f79bb596a3ab5e0a2f338ea43b5be53307
Llenrep <lppernell1998@gmai.com>
2019-01-07 14:43:20 -0500

    okay the pages work again

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/verify", function(req, res) {
@@ -24,12 +24,12 @@ module.exports = function(app) {
 
   })
 
-  app.get("/login", function(req, res) {
+  app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
